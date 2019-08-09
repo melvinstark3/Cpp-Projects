68eaf01da2ad091d0aba5e705557560218b8399a
“melvinstark3” <kartik40814902018@msi-ggsip.org>
2019-08-09 17:27:58 +0530

    c++: Temprature Convertor

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  
+  double tempf;
+  double tempc;
+  
+  // Ask the user
+  std::cout << "Enter the temperature in Fahrenheit.";
+  std::cin >> tempf;
+  
+  tempc = (tempf - 32) / 1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+}
