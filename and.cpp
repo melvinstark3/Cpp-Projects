982e3ee6446f2d37a60dcc63f367f7ae66a1ca58
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-09 20:08:53 +0530

    c++: And statement

---
+++ and.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  
+  bool hunger = true;
+  bool anger = true;
+  
+  // Write the code below:
+  if (hunger==true && anger==true){
+    std::cout << "RRRUUUUUUUUUNNNNNN";
+  }
+  
+}
