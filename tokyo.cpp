83ec8eaba34751dceab21d925df65451472e790d
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-10 15:46:06 +0530

    c++: Vectors 1st Program
    Question: Suppose the Tokyo Subway costs are as follows:
    
    Ticket	Adult	Child
    24-hour	¥800	¥400
    48-hour	¥1200	¥600
    72-hour	¥1500	¥750
    
    Initialize the subway_child vector & subwat_adult vector.

---
+++ tokyo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+//store adult fare   
+  std::vector<double> subway_adult = {800, 1200, 1500};
+
+//Store Child fare  
+  std::vector<double> subway_child = {400, 600 ,750};
+  
+}
