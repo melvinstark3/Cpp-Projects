89cf1a0c203a1631f8c2e6ed5937c176925c4ae5
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-10 19:32:28 +0530

    c++: Insert elements using .push_back()
    .push_back()
    To add a new element to the “back”, or end of the vector, we can use the .push_back() function.

--- strivec.cpp
+++ strivec.cpp
@@ -5,4 +5,9 @@ int main() {
   
   std::vector<std::string> last_jedi;
   
+  last_jedi.push_back("kylo");
+  last_jedi.push_back("rey");
+  last_jedi.push_back("luke");
+  last_jedi.push_back("finn");
+  
 }
