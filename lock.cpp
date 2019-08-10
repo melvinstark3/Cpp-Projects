6e361e97db88e8bda49af509d6394415a9e18d26
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-10 13:20:15 +0530

    c++: add PIN checker
    C++ program to check if the Pin is correct or not

---
+++ lock.cpp
@@ -0,0 +1,32 @@
+ #include <iostream>
+
+int main() {
+
+  int defpin=1541;
+  int pin,flag=1;
+  int tries = 3;
+
+  std::cout << "Lock Room\n"; //Message
+
+  while (tries != 0 ) { //initialize loop
+        std::cout << "Enter your PIN: "; //input by user
+        std::cin >> pin;
+
+    if (pin == defpin) { //Success condition
+
+        std::cout << "PIN accepted!\n";
+        std::cout << "You now have access.\n";
+        break;
+    }
+
+    else { //failcase
+                if (tries == 1){
+                  std::cout << "User Locked for 24 hours\n";
+                }
+                else {
+                std::cout << "Wrong Pin. Try Again\n";
+              }
+              }
+        tries--;
+ }
+}
