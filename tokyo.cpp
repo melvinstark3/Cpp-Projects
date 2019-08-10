854d18ef34952a15ab3cef7cfd46815407507130
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-10 16:01:10 +0530

    c++: Accessing Elements of vectors

--- tokyo.cpp
+++ tokyo.cpp
@@ -9,4 +9,9 @@ int main() {
 //Store Child fare  
   std::vector<double> subway_child = {400, 600 ,750};
   
+//access the data
+  std::cout << "Ticket  Adult Child\n";
+  std::cout << "24-hour " << subway_adult[0] << "   " << subway_child[0] << "\n";
+  std::cout << "48-hour " << subway_adult[1] << "  " << subway_child[1] << "\n";
+  std::cout << "72-hour " << subway_adult[2] << "  " << subway_child[2] << "\n";
 }
