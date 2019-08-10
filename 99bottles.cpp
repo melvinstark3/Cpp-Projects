4f8f3345437d518d9518aef9ec3d1632ca0cfb35
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-10 15:22:20 +0530

    c++: add 99 bottles program
    Question: Use for loop and Write a 99bottles.cpp program that prints the verses of the “99 Bottles” song. Each stanza goes something like this:
    
    i bottles of pop on the wall.
    Take one down and pass it around.
    i-1 bottles of pop on the wall.

---
+++ 99bottles.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+
+	for (int i=99; i>0; i--){
+    std::cout << i << "bottles of pop on the wall.\n" << "Take one down and pass it around.\n" << i-1 << "bottles of pop on the wall.\n";
+  } 
+}
