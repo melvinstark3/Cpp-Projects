45f0caa1a2e8aa3d957619851e063671521f49d0
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-11 22:47:59 +0530

    c++: functions
    A C++ function is comprised of two distinct parts:
    
    Declaration: this includes the function’s name, what the return type is, and any parameters (if the function will accept input values, known as arguments).
    Definition: also known as the body of the function, this contains the instructions for what the function is supposed to do.
    
    ************************
    SYNTAX: return_type function_name( any, parameters, you, have ) {
       // Code block here
       return output_if_there_is_any;
    }
    ************************
    In this program we defined a void function oscar_wilde_quote() that prints “The highest, as the lowest, form of criticism is a mode of autobiography.” to the output terminal and Inside of main(), called oscar_wilde_quote().

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Define function i.e oscar_wilde_quote() below
+void oscar_wilde_quote(){
+  std::cout << "The highest, as the lowest, form of criticism is a mode of autobiography.\n";
+}
+
+int main() {
+  
+  // Call your function here
+  oscar_wilde_quote();
+  
+}
