fa83add18ea57a8f26d98f840dc6e57c91d46f8b
Melvin <47421908+melvinstark3@users.noreply.github.com>
2019-08-18 00:32:31 +0530

    c++: Elaborated Palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -3,17 +3,20 @@
 // Define is_palindrome() here:
 bool is_palindrome(std::string text) {
   
-  std::string reversed_text = "";
+  std::string reversed_text = ""; //Define empty string reversed_text
   
+  //used .size() operator to get the size of the text and 
+  //use for loop to fill up empty string by iterating the 
+  //text from backwards
   for (int i = text.size() - 1; i >= 0; i--) {
-    reversed_text += text[i];
+    reversed_text += text[i]; //fill up empty string
   }
   
   if (reversed_text == text) {
-    return true;
+    return true; //true condition. Output will be 1
   }
   
-  return false;
+  return false; //false condition. Output will be 0
   
 }
 
