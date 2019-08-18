ac18f19f93aa4f1aa6c5490c2e6cbb806af08168
Melvin <kartik3162@gmail.com>
2019-08-18 23:41:08 +0530

    c++: Create Headers file for Functions Declarations
    Rather than declaring functions again again we can declare the functions in a header file with an extension of ".hpp" or ".h" and include the header file  only in the code.

--- multifile_programs/3functions/main.cpp
+++ multifile_programs/3functions/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-
-// Add declarations here:
-double average(double num1, double num2);
-int tenth_power(int num);
-bool is_palindrome(std::string text);
+#include "3fc.h"
 
 int main() {
   
