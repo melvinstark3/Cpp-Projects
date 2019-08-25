327485883c3bcc6565b614caeadefbb903c3515d
Melvin <kartik3162@gmail.com>
2019-08-26 00:28:01 +0530

    c++: Creating Objects in Classes
    To use the data and access functions defined in the class, you need to create objects.
    Syntax: 
    ClassName ObjectName;
    
    Accessing data members and member functions: 
    The data members and member functions of class can be accessed using the dot(‘.’) operator with the object
    Syntax: ObjectName.ClassMember
    
    In this Code we did the following:
    * In music.cpp, instantiated (created) a new song electric_relaxation(ObjectName) in main().
    * Used .add_title() method to add a title to electric_relaxation: "Electric Relaxation".
    * Used .get_title() to retrieve electric_relaxations title and print it to the terminal.

--- class/song/music.cpp
+++ class/song/music.cpp
@@ -3,4 +3,10 @@
 
 int main() {
 
+	//Create an Object
+  Song electric_relaxation;
+  
+  electric_relaxation.add_title("Electric Relaxation");
+  std::cout << electric_relaxation.get_title();
+  
 }
