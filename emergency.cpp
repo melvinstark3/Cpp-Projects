36cbd63eaac41f69eeab8a1ffa122e018e1ccbd4
sumdud3 <42804684+sumdud3@users.noreply.github.com>
2019-10-05 12:04:15 +0530

    Function call implementation with vector
    We have passed values from a vector to a function using call by Value

--- emergency.cpp
+++ emergency.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <vector>
 
 // Defined void function with string parameter. here emergency_number is the parameter
-void get_emergency_number(std::string emergency_number) {
+void get_emergency_number(int emergency_number) {
   
   //print and return
   std::cout << "Dial " << emergency_number;
@@ -9,14 +10,14 @@ void get_emergency_number(std::string emergency_number) {
 }
 
 int main() {
-  
+  srand(time(NULL));
   // Original emergency services number 
   std::string old_emergency_number = "999";
   
-  // new number
-  std::string new_emergency_number = "0118 999 881 999 119 725 3";
+  // new numbers within an indexed vector
+  std::vector<int> new_emergency_number = {0118, 999, 881, 999, 119, 725, 3};
   
   //call the function 
-  get_emergency_number(new_emergency_number); //here new_emergency_number in the argument
+  get_emergency_number(new_emergency_number[rand()]); //here new_emergency_number in the argument
   
 }
