956eb47d87e97e8307ff5f1e03dc4a7a15fd4d2e
sumdud3 <42804684+sumdud3@users.noreply.github.com>
2019-10-05 12:18:58 +0530

    MInor changes
    Obsolete line removed and comments added

--- 99bottles.cpp
+++ 99bottles.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-int main() {
-
-	for (int i=99; i>0; i--){
-    std::cout << i << "bottles of pop on the wall.\n" << "Take one down and pass it around.\n" << i-1 << "bottles of pop on the wall.\n";
-  } 
+int main() 
+{
+	for (int i=99; i>0; i--) //for loop iteration on decremental series
+	{
+    		std::cout << i << "bottles of pop on the wall.\n" << "Take one down and pass it around.\n"; // generated for every ith value
+  	} 
 }
