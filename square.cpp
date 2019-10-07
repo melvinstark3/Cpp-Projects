ff40a564fe86560f0648b42648f2af9bec095775
Ankit Jee <ankitjee2000@gmail.com>
2019-10-07 15:10:56 +0530

    Corrected various things
    Added proper return type and  made the program based upon user input

--- square.cpp
+++ square.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-
-int main() {
+using namespace std;
+int main() 
+{
   
-  int i = 0;
+  int i = 0,n;
   int square = 0;
   
-  while (i !=10){
-    square = i*i;
-    std::cout << i << " " << square << "\n";
-    i++;
+  //Get input from users
+  std::cout<<"Enter the ending numbers : ";
+  std::cin>>n;
   
+  //Code to show squares
+  while (i <=n)
+  {
+    square = i*i;
+    std::cout << "Square of " << i <<" : " <<square << "\n";
+    i++;
+  }
+  
+  return 0;
+}
